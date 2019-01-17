c07c3e678d58037b4cd03964e0ed5d004afee87d
gangwar.yogendra <gangwar.yogendra@gmail.com>
2019-01-18 02:45:01 +0530

    Adding new file

---
+++ myFirstTestCpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T, size_t count>
+size_t getArrSize(T (&arr)[count])
+{
+   return (sizeof(arr)/sizeof(T));
+}
+
+int main()
+{
+   int arr[] = {1, 2, 3, 4};
+   cout<<getArrSize<int>(arr)<<endl;
+}
+
